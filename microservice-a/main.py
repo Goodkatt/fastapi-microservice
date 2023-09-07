07e4e2cc2f102f3fb5ae76aa46db4c37d47082c7
Görkem Altunay <gorkem444altunay@hotmail.com>
2023-09-07 22:05:08 +0300

    new routes

--- microservice-a/main.py
+++ microservice-a/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World from microservice A"}
+
+@app.get("/home")
+async def home():
+    return {"Homepage of microsservice A"}
--- microservice-b/main.py
+++ microservice-b/main.py
@@ -6,3 +6,6 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World from microservice B"}
+@app.get("/home")
+async def home():
+    return {"Homepage of microsservice B"}
